ee3d2a75600bc0e742deeb820f21ac3ff8b33fa3
SrPhilippe <philippehenrique@outlook.com>
2022-10-19 19:21:44 -0300

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector('.cc-bg svg > g g:first-child path')
+const ccBgColor02 = document.querySelector('.cc-bg svg > g g:last-child path')
+
+ccBgColor01.setAttribute('fill', 'green')
+ccBgColor02.setAttribute('fill', 'red')
